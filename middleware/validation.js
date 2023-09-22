dcfb8f1f3116406c2da82df397b595125f6c4caf
aprimanfikri <aprmnfkr@gmail.com>
2023-09-22 23:28:39 +0700

    Add custom validation message

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,13 +1,29 @@
 const validate = (req, res, next) => {
   const { plate, manufacture, model } = req.body;
-
-  if (!plate || !manufacture || !model) {
+  const missing = [];
+  if (!plate) {
+    missing.push("Plate");
+  }
+  if (!manufacture) {
+    missing.push("Manufacture");
+  }
+  if (!model) {
+    missing.push("Model");
+  }
+  let message = "";
+  if (missing.length === 1) {
+    message = `${missing[0]} harus diisi`;
+  } else if (missing.length === 2) {
+    message = `${missing[0]} dan ${missing[1]} harus diisi`;
+  } else if (missing.length === 3) {
+    message = `${missing[0]}, ${missing[1]}, dan ${missing[2]} harus diisi`;
+  }
+  if (missing.length > 0) {
     return res.status(400).json({
       status: "failed",
-      message: "Plate, manufacture, dan model harus diisi",
+      message,
     });
   }
-
   next();
 };
 
